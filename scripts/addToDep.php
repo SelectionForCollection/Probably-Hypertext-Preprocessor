315c77b994e5f34c89031835282c0e8c4c5d021e
Dmitry Lisyutin <lisyutin.dima@mail.ru>
2022-12-10 08:37:28 +0300

    dropdown v2.5.1

--- scripts/addToDep.php
+++ scripts/addToDep.php
@@ -4,8 +4,9 @@
 
     $dev = $_POST["firstDevs"];
     $dep = $_POST["firstDeps"];
-    
+    echo 1;
     $data = $pdo->query("SELECT idDepartment FROM department WHERE titleDepartment = '" . $dep . "';")->fetchAll();
+    echo 2;
     $pdo->query("UPDATE developer SET department = " . $data[0][0] . " WHERE developer.nickname = '" . $dev . "';");
 
     header("Location: ../administration.php");
