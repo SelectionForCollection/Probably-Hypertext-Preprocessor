ef8edfde67619d0fb8958984273f8aa08c705f62
Dmitry Lisyutin <lisyutin.dima@mail.ru>
2022-12-10 08:49:49 +0300

    stupid Sellya, sorry $_POST

--- scripts/addToDep.php
+++ scripts/addToDep.php
@@ -1,11 +1,8 @@
 <?php
-
-    print_r($_POST["firstDevs"]);
     $dev = $_POST["firstDevs"];
-    print_r($dev);
     $dep = $_POST["firstDeps"];
 
-    require_once("scripts/boot.php");
+    require_once("boot.php");
 
     $data = $pdo->query("SELECT idDepartment FROM department WHERE titleDepartment = '" . $dep . "';")->fetchAll();
     $pdo->query("UPDATE developer SET department = " . $data[0][0] . " WHERE developer.nickname = '" . $dev . "';");
