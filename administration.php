f0a1c6770263f1379fec118fad07fb91cd81477e
Dmitry Lisyutin <lisyutin.dima@mail.ru>
2022-12-10 07:47:07 +0300

    dropdown v2

--- administration.php
+++ administration.php
@@ -8,15 +8,22 @@ if (isset($_COOKIE["page_control"])) {
 
 require_once('scripts/boot.php');
 
-
-$dropdown = array("Pizza1", "Pizza2", "Pizza3");
-$str='<select name="drop_down" size="1">';
- 
-for($i = 0, $c = count($dropdown); $i < $c; $i++) {
-     $str.='<option value="'.$i.'">'.$dropdown[$i].' </option>';
- }
-$srt.='</select>';
- 
-echo $str;
-
 ?>
+<html>
+    <head>
+        <title>Administration</title>
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="styles/administration.css" type="text/css"/>
+    </head>
+    <body>
+        <select>
+            <?php
+                $data = $pdo->query("SELECT nickname FROM developer;")->fetchAll();
+                foreach ($data[0] as $n=>$opt) {
+                     printf('<option value="%s">%s</option>', $n, $opt);
+                }
+            ?>
+        </select>
+    </body>
+</html>
