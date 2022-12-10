8b82bf0a73b72dde31ef4622aab60456515069e9
Dmitry Lisyutin <lisyutin.dima@mail.ru>
2022-12-10 08:19:09 +0300

    dropdown v2.4

--- administration.php
+++ administration.php
@@ -17,23 +17,26 @@ require_once('scripts/boot.php');
         <link rel="stylesheet" href="styles/administration.css" type="text/css"/>
     </head>
     <body>
-        <h1>Прикрепить </h1>
-        <select>
-            <?php
-                $data = $pdo->query("SELECT nickname FROM developer;")->fetchAll();
-                foreach ($data as $el) {
-                    echo '<option value="' . $el[0] . '">' . $el[0] . '</option>';
-                }
-            ?>
-        </select>
-        <h1> к отделу </h1>
-        <select>
-            <?php
-                $data = $pdo->query("SELECT titleDepartment FROM department;")->fetchAll();
-                foreach ($data as $el) {
-                    echo '<option value="' . $el[0] . '">' . $el[0] . '</option>';
-                }
-            ?>
-        </select>
+        <form action="scripts/addToDep.php" method="POST">
+            <h1>Добавить </h1>
+            <select name="firstDevs">
+                <?php
+                    $data = $pdo->query("SELECT nickname FROM developer;")->fetchAll();
+                    foreach ($data as $el) {
+                        echo '<option value="' . $el[0] . '">' . $el[0] . '</option>';
+                    }
+                ?>
+            </select>
+            <h1> к отделу </h1>
+            <select name="firstDeps">
+                <?php
+                    $data = $pdo->query("SELECT titleDepartment FROM department;")->fetchAll();
+                    foreach ($data as $el) {
+                        echo '<option value="' . $el[0] . '">' . $el[0] . '</option>';
+                    }
+                ?>
+            </select>
+            <button>Добавить</button>
+        </form>
     </body>
 </html>
---
+++ scripts/addToDep.php
@@ -0,0 +1,7 @@
+<?php
+
+    require_once("scripts/boot.php");
+
+    echo $_POST["firstDevs"];
+    echo $_POST["firstDeps"];
+?>
