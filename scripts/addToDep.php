66cfbe968577de54a96d2d85f9e7b8430abb0f8c
Dmitry Lisyutin <lisyutin.dima@mail.ru>
2022-12-10 08:25:26 +0300

    dropdown 2.4.2????

--- scripts/addToDep.php
+++ scripts/addToDep.php
@@ -1,8 +1,9 @@
 <?php
 
-    require_once("scripts/boot.php");
+    // require_once("scripts/boot.php");
 
-    // echo $_POST["firstDevs"];
-    // echo $_POST["firstDeps"];
-    var_dump($_POST);
+    // // echo $_POST["firstDevs"];
+    // // echo $_POST["firstDeps"];
+    // var_dump($_POST);
+    print_r($_POST);
 ?>
