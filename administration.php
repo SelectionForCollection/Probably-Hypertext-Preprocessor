390f18902aa1f68aaaee7a763c26fab90df78112
Dmitry Lisyutin <lisyutin.dima@mail.ru>
2022-12-10 08:02:54 +0300

    dropdown v2.3

--- administration.php
+++ administration.php
@@ -17,11 +17,21 @@ require_once('scripts/boot.php');
         <link rel="stylesheet" href="styles/administration.css" type="text/css"/>
     </head>
     <body>
+        <h1>Прикрепить </h1>
         <select>
             <?php
                 $data = $pdo->query("SELECT nickname FROM developer;")->fetchAll();
                 foreach ($data as $el) {
-                    echo '<option value="' . $el["nickname"] . '">' . $el["nickname"] . '</option>';
+                    echo '<option value="' . $el[0] . '">' . $el[0] . '</option>';
+                }
+            ?>
+        </select>
+        <h1> к отделу </h1>
+        <select>
+            <?php
+                $data = $pdo->query("SELECT titleDepartment FROM department;")->fetchAll();
+                foreach ($data as $el) {
+                    echo '<option value="' . $el[0] . '">' . $el[0] . '</option>';
                 }
             ?>
         </select>
