8f81a251757315e8f1a810e882a0515598d6f9b2
Dmitry Lisyutin <lisyutin.dima@mail.ru>
2022-12-10 08:40:26 +0300

    fucking $_POST

--- scripts/addToDep.php
+++ scripts/addToDep.php
@@ -1,12 +1,13 @@
 <?php
 
+    print_r($_POST);
+
     require_once("scripts/boot.php");
 
     $dev = $_POST["firstDevs"];
     $dep = $_POST["firstDeps"];
-    echo 1;
+
     $data = $pdo->query("SELECT idDepartment FROM department WHERE titleDepartment = '" . $dep . "';")->fetchAll();
-    echo 2;
     $pdo->query("UPDATE developer SET department = " . $data[0][0] . " WHERE developer.nickname = '" . $dev . "';");
 
     header("Location: ../administration.php");
